1a7a1155adce784b8f3d5d740e7f000b61529e2d
Your Name <you@example.com>
2020-09-09 00:42:50 +0300

    fix: creat dream team PASS

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (!members || typeof members !== 'object' || !members.length) {return false;}
+    const result = members.
+      reduce((stringMembers, item) => {
+          if (item && typeof item === 'string') {
+          return stringMembers.concat(item.trim().toUpperCase());
+          }
+          else return stringMembers;
+       }, []).
+      sort().
+      reduce((res, item) => {
+        return res += item[0]
+      },'');
+    if (result) {
+      return result;
+    }
+    else return false;
 };
