5b59663a926e9cf6a16669337f4b17847ed4c2d9
Your Name <you@example.com>
2020-09-09 09:41:21 +0300

    fix: I know what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (!date || typeof date !== 'object') {
+        return 'Unable to determine the time of year!';
+    }
+    const milisec = date.getTime();
+    const testDate = new Date(milisec);
+    const month = ([0,1,2,3,4,5,6,7,8,9,10,11]).indexOf(date.getMonth());
+    if (testDate.getMonth() !== date.getMonth() ||
+        testDate.getFullYear() !== date.getFullYear() ||
+        testDate.getDay() !== date.getDay()) {return 'THROWN';}
+    if ( month<=11 && month >= 0) {
+        switch (month) {
+            case 0:
+            case 1:
+            case 11:
+                return 'winter';
+            case 2:
+            case 3:
+            case 4:
+                return 'spring';
+            case 5:
+            case 6:
+            case 7:
+                return 'summer';
+            case 8:
+            case 9:
+            case 10:
+                return 'fall';
+        }
+    }
+    else return 'THROWN';
 };
