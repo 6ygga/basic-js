5fd8327620f51131f4fad636623b29be4d56c829
Your Name <you@example.com>
2020-09-11 22:27:55 +0300

    fix: Transformer DONE (Autobots forever)

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let arra = [];
+  let res = [];
+  arra = arr.concat(arra);
+  if (arr.length === 0 || arr[0] === undefined) return [];
+  let done = false;
+  let index = 0;
+  while (!done) {
+    switch (arra[index]) {
+      case '--discard-next':
+        if (index === arra.length-1) {done = true; break; }
+        if (equal(arra[index+1])) {break;}
+        arra.splice(index+1,1); break;
+      case '--discard-prev':
+        if (index === 0) {break; }
+        if (equal(arra[index - 1])) {break;}
+        res.pop(); break;
+      case '--double-next':
+        if (index === arra.length - 1) {done = true; break; }
+        if (equal(arra[index+1])) {break;}
+        res.push(arra[index+1]); break;
+      case '--double-prev':
+        if (index === 0) {break; }
+        if (equal(arra[index-1])) {break;}
+        res.push(arra[index - 1]); break;
+      default: res.push(arra[index]); break;
+    }
+    if (++index >= arra.length) break;
+  }
+  return res; //.concat(arr)
 };
+
+function equal(str) {
+  return str === '--discard-next' || str === '--discard-prev' || str === '--double-next' || str === '--double-prev';
+}
