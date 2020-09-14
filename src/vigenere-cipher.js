e06930088b26d982f80a4bb6a5997bcf0eaed3bb
Your Name <you@example.com>
2020-09-14 10:50:26 +0300

    fix: cipher was started but not fished

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  direction = true;
+  alphabet = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
+  constructor(param){
+    this.direction = (!param ? false : true);
+  }
+  encrypt(message, key) {
+    let keystring = key;
+    while (keystring.length < message.length) {keystring += key;}
+    keystring = keystring.slice(0, message.length);
+    return message.split('').reduce((res, item) => {
+      return res += letterArr(item)[this.alphabet.indexOf(item)];
+    },'')
+  }
+  decrypt(encryptedMessage, key) {
+
+  }
+
+  letterArr(letter) {
+    return [].concat(this.alphabet.filter(item => item >= letter), this.alphabet.filter(item => item < letter));
   }
 }
 
